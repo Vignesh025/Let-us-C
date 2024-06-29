55187731df726d447d83d0015756ab1f23f8c7aa
Vignesh025 <vigneshjayakumar9221@gmail.com>
2024-06-29 20:39:47 +0530

    change

--- Strings/Examples/substring.c
+++ Strings/Examples/substring.c
@@ -1,37 +1,50 @@
 /* To extract a substring from a string */ 
-# include <stdio.h> 
-# include <stdlib.h> 
-# include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main( ) 
-{ 
-    char str[ 20 ], news[ 20 ] ; 
-    char *s, *t ; 
-    int pos, n, i ; 
-    printf ( "\nEnter a string: " ) ; 
-    scanf ( "%s", str ) ; 
-    fflush(stdin);
-    printf ( "Enter position and no. of characters to extract: " ) ; 
-    scanf ( " %d %d", &pos, &n ) ; 
-    s = str ; 
-    t = news ; 
-    if ( pos < 0 || pos > strlen ( str ) ) 
-    { 
-        printf ( "Improper position value" ) ; 
-        exit ( 1 ) ; 
-    } 
-    if ( n < 0 ) 
-        n = 0 ; 
-    if ( n > strlen ( str ) ) 
-        n = n - strlen ( str ) - 1 ; 
-    s = s + pos ; 
-    for ( i = 0 ; i < n ; i++ ) 
-    { 
-        *t = *s ; 
-        s++ ; 
-        t++ ; 
-    } 
-    *t = '\0' ; 
-    printf ( "The substring is: %s\n", news ) ; 
-    return 0 ; 
-} 
+int main()
+{
+    char str[20], news[20];
+    char *s, *t;
+    int pos, n, i;
+
+    printf("\nEnter a string: ");
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        printf("Error reading input\n");
+        return 1;
+    }
+    str[strcspn(str, "\n")] = 0; // Remove newline if present
+
+    printf("Enter position and no. of characters to extract: ");
+    if (scanf("%d %d", &pos, &n) != 2) {
+        printf("Error reading position and number of characters\n");
+        return 1;
+    }
+
+    s = str;
+    t = news;
+
+    if (pos < 0 || pos > strlen(str))
+    {
+        printf("Improper position value\n");
+        return 1;
+    }
+
+    if (n < 0)
+        n = 0;
+    if (n > strlen(str) - pos)
+        n = strlen(str) - pos;
+
+    s = s + pos;
+    for (i = 0; i < n; i++)
+    {
+        *t = *s;
+        s++;
+        t++;
+    }
+    *t = '\0';
+
+    printf("The substring is: %s\n", news);
+    return 0;
+}
